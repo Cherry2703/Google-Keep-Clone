a06e02f1575ce091d0f96fefab67ae7ac07c2da4
Cherry2703 <ramcharanamr2408@gmail.com>
2024-12-02 20:08:49 +0530

    made changes in frontend archive and remainders page

--- google-keep-frontend/src/components/Labels/index.js
+++ google-keep-frontend/src/components/Labels/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import './index.css'
+
 const Labels = () => {
   return (
-    <div>Labels</div>
+    <div className='labels-main-container'>
+      <div>
+        <h2>Need to work on Labels Page . So go for another pages .....</h2>
+      </div>
+    </div>
   )
 }
 
--- google-keep-frontend/src/components/Remainders/index.js
+++ google-keep-frontend/src/components/Remainders/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import './index.css'
+
 const Remainders = () => {
   return (
-    <div>Remainders</div>
+    <div className='remainders-main-container'>
+      <div>
+        <h2>Need to work on Remainders Page . So go for another pages .....</h2>
+      </div>
+    </div>
   )
 }
 
